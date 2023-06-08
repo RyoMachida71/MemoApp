5b52cd85276b3ab8b95a4a3d57b90ca53736c207
machida <banzyonosite@gmail.com>
2023-06-08 22:48:15 +0900

    added search and replace function by using regex regex replacement is incomplete
    todo : fix regex replacement bug

--- MemoApp/Search/RegexSearcher.cs
+++ MemoApp/Search/RegexSearcher.cs
@@ -5,39 +5,69 @@ namespace MemoApp.Search {
     public class RegexSearcher : ISearcher {
         private ISearchTarget FSearchTarget;
         private SearchArg FArg;
-        private Regex FRegex;
-        private StringComparison FStrignComparison;
+        private MatchCollection FMatches;
+
         public RegexSearcher(ISearchTarget vSearchTarget) {
             FSearchTarget = vSearchTarget;
         }
+
         public void PrepareSearch(SearchArg vArg) {
             FArg = vArg;
-            RegexOptions wOptions = FArg.IsDistinguishCase ? RegexOptions.None | RegexOptions.Compiled : RegexOptions.IgnoreCase | RegexOptions.Compiled;
-            FRegex = new Regex(vArg.SearchText, wOptions);
-            FStrignComparison = vArg.IsDistinguishCase ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
         }
 
         public int SearchForward() {
-            var wOffset = FSearchTarget.SelectedText.Equals(FArg.SearchText, FStrignComparison) ? 1 : 0;
-            var wMatch = FRegex.Match(FSearchTarget.Text, FSearchTarget.SelectionStart + wOffset);
-            if (!wMatch.Success) return -1;
-            FSearchTarget.Select(wMatch.Index, wMatch.Length);
-            return wMatch.Index;
+            foreach (Match wMatch in FMatches) {
+                if (wMatch.Index <= FSearchTarget.SelectionStart) continue;
+                FSearchTarget.Select(wMatch.Index, wMatch.Length);
+                return wMatch.Index;
+            }
+            return -1;
         }
+
         public int SearchBackward() {
+            for (int i = FMatches.Count - 1; i >= 0; --i) {
+                var wMatch = FMatches[i];
+                if (wMatch.Index >= FSearchTarget.SelectionStart) continue;
+                FSearchTarget.Select(wMatch.Index, wMatch.Length);
+                return wMatch.Index;
+            }
             return -1;
         }
+
         public int SearchAll() {
+            var wOriginalPosition = FSearchTarget.SelectionStart;
+            FMatches = Regex.Matches(FSearchTarget.Text, FArg.SearchText, RegexOptions.Compiled | RegexOptions.Multiline);
+            foreach (Match wMatch in FMatches) {
+                FSearchTarget.SelectionStart = wMatch.Index;
+                FSearchTarget.SelectionLength = wMatch.Length;
+                FSearchTarget.HighlightSelectedText();
+            }
+            FSearchTarget.SelectionStart = wOriginalPosition;
             return -1;
         }
+
         public int ReplaceForward() {
-            return -1;
+            var wIndex = this.SearchForward();
+            FSearchTarget.SelectedText = Regex.Unescape(FArg.ReplaceText);
+            return wIndex;
         }
+
         public int ReplaceBackward() {
-            return -1;
+            var wIndex = this.SearchBackward();
+            int wOriginalPosition = FSearchTarget.SelectionStart;
+            FSearchTarget.SelectedText = Regex.Unescape(FArg.ReplaceText);
+            FSearchTarget.SelectionStart = wOriginalPosition;
+            return wIndex;
         }
+
         public int ReplaceAll() {
-            return -1;
+            var wOriginalPosition = FSearchTarget.SelectionStart;
+            int wIndex = FSearchTarget.SelectionStart = FSearchTarget.Text.Length - 1;
+            while (wIndex >= 0) {
+                wIndex = this.ReplaceBackward();
+            }
+            FSearchTarget.Select(wOriginalPosition, 0);
+            return wIndex;
         }
     }
 }
