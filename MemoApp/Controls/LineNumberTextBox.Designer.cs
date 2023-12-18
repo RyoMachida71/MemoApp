dd72469852e80395d0b0ccba5a6c305b2161d6dc
machida <banzyonosite@gmail.com>
2023-12-18 22:33:38 +0900

    create LineNumTextBox

---
+++ MemoApp/Controls/LineNumberTextBox.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MemoApp.Controls {
+    partial class LineNumberTextBox {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.customTextBox = new MemoApp.CustomTextBox();
+            this.SuspendLayout();
+            // 
+            // customTextBox
+            // 
+            this.customTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customTextBox.BackColor = System.Drawing.Color.Black;
+            this.customTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.customTextBox.Font = new System.Drawing.Font("ＭＳ ゴシック", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.customTextBox.ForeColor = System.Drawing.Color.White;
+            this.customTextBox.Location = new System.Drawing.Point(62, 0);
+            this.customTextBox.Name = "customTextBox";
+            this.customTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.customTextBox.Size = new System.Drawing.Size(237, 238);
+            this.customTextBox.TabIndex = 0;
+            this.customTextBox.Text = "";
+            // 
+            // LineNumberTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.customTextBox);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "LineNumberTextBox";
+            this.Size = new System.Drawing.Size(299, 238);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomTextBox customTextBox;
+    }
+}
---
+++ MemoApp/Controls/LineNumberTextBox.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoApp.Controls {
+    public partial class LineNumberTextBox : UserControl {
+        private const int LeftMargin = 40;
+        private const int DrawingOffset = 2;
+
+        private readonly Point _bottomLeft;
+        private readonly Brush _fontBrush;
+        private StringFormat _lineFormat;
+        private int _lastLineCount;
+
+        public CustomTextBox TextBox => customTextBox;
+        public LineNumberTextBox() {
+            InitializeComponent();
+            customTextBox.Initialize();
+            this.Dock = DockStyle.Fill;
+            this.DoubleBuffered = true;
+            this.Location = new Point(0, 0);
+            this.customTextBox.Location = new Point(LeftMargin, 0);
+            this.customTextBox.TextChanged += (s, e) => this.UpdateLineCount();
+            this.customTextBox.VScroll += (s, e) => this.UpdateLineCount();
+            this.Paint += LineNumberTextBox_Paint;
+
+            _bottomLeft = new Point(0, this.ClientRectangle.Height);
+            _fontBrush = new SolidBrush(customTextBox.ForeColor);
+            _lineFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+        }
+
+        private void UpdateLineCount() {
+            if (customTextBox.Lines.Length != _lastLineCount) {
+                _lastLineCount = customTextBox.Lines.Length;
+                this.Invalidate();
+            }
+        }
+
+        private void LineNumberTextBox_Paint(object sender, PaintEventArgs e) {
+            e.Graphics.Clear(customTextBox.BackColor);
+
+            var visibleFirstLine = GetVisibleLine(Point.Empty);
+            var visibleLastLine = GetVisibleLine(_bottomLeft);
+            for (int i = visibleFirstLine; i <= visibleLastLine + 1; ++i) {
+                var yPos = GetYPositionOfLine(i);
+                if (yPos == -1) continue;
+                e.Graphics.DrawString((i + 1).ToString(), this.Font, Brushes.White, new RectangleF(0, yPos + DrawingOffset, LeftMargin, this.Font.Height), _lineFormat);
+            }
+        }
+
+        private int GetVisibleLine(Point pos) {
+            var index = customTextBox.GetCharIndexFromPosition(pos);
+            return customTextBox.GetLineFromCharIndex(index);
+        }
+
+        private int GetYPositionOfLine(int line) {
+            var index = customTextBox.GetFirstCharIndexFromLine(line);
+            if (index == -1) return index;
+            return customTextBox.GetPositionFromCharIndex(index).Y;
+        }
+    }
+}
